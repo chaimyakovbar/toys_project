0c0fc96f01035cfa3667be95499bc420353debc5
chaimyakovbar <134381405+chaimyakovbar@users.noreply.github.com>
2023-09-21 20:09:54 +0300

    Add files via upload

--- confidRoutes.js
+++ confidRoutes.js
@@ -2,6 +2,6 @@ const usersR = require("./users");
 const toysR = require("./toys");
 
 exports.router = (app) => {
-  app.use("/", toysR);
+  app.use("/toys", toysR);
   app.use("/users", usersR);
 };
